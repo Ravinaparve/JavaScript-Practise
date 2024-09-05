dfdfa565520ccc54f83de57eb764342d87d60ccd
Ravina <parveravina@gmail.com>
2024-09-05 21:23:43 +0530

    Update let_var_const.js

--- Miscellaneous/Interview_Qs/let_var_const.js
+++ Miscellaneous/Interview_Qs/let_var_const.js
@@ -7,7 +7,9 @@ console.log(greet); //Welcome
 
 
 //..............................LET & CONST ..............................................
-// same let & const variable defined in different scopes will not be error because both are treated as different variables. the means they are 2 different variables even if the name is same
+// same let & const variable defined in different scopes will not be error because both are treated as different variables. 
+//the means they are 2 different variables even if the name is same.
+
 let greet = "Hi";
 if (true) {
     let greet = "Welcome";
