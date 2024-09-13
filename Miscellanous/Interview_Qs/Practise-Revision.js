d6955780b717873c4ee89920dd0dc729ad8b3f1e
Ravina <parveravina@gmail.com>
2024-09-13 10:15:21 +0530

    Update Practise-Revision.js

--- Miscellanous/Interview_Qs/Practise-Revision.js
+++ Miscellanous/Interview_Qs/Practise-Revision.js
@@ -7,7 +7,8 @@
 console.log(isFinite(null)); // true
 //The null value is treated as 0 when used in numerical contexts, and since 0 is a finite number, isFinite(null) returns true.
 
-console.log(+" ");
+console.log(+" "); //o/p: 0
+//An empty string is then converted to 0 when using the unary plus operator. So, +" " evaluates to 0.
 
 console.log(isFinite(" ")); // true
 
