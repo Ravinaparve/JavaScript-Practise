43c5b55e2b3dafffed31477e7d9cd85a93557e96
Ravina <parveravina@gmail.com>
2024-09-13 10:58:27 +0530

    Updated deep_shallowCopy.js

--- Miscellanous/Interview_Qs/deep_shallowCopy.js
+++ Miscellanous/Interview_Qs/deep_shallowCopy.js
@@ -15,24 +15,33 @@ let obj1 = {
     }
 }
 
-// shallow copy
+// SHALLOW COPY
+//this is a shallow copy, meaning that the top-level properties are copied by value, but nested objects (like company) are copied by reference.
 let obj2 = { ...obj1 };
 obj2.company[1].company_name = "Google";
 console.log(obj1); // changed here as well
 console.log(obj2);
 
-console.log("..............................................")
+//obj2.company[1].company_name = "Google"; modifies the company_name property of company[1] in obj2.
+//Since company is a nested object and only a reference to it is copied, both obj1 and obj2 still point to the same company object. 
+//Therefore, this modification affects both obj1 and obj2.
 
 
+.................................................................................................................................................
 
 
-//Deep Copy:
+// DEEP COPY ...
+
 let obj3 = JSON.parse(JSON.stringify(obj1));
 obj3.company[1].company_name = "Flipkart";
-console.log("obj3==>", obj3)
-console.log("obj1==>", obj1); // changed here as well
 
+console.log("obj3==>", obj3) //meaning that changes to one obj will not affect other obj
 
+console.log("obj1==>", obj1); // obj3 reflects the change to "Flipkart", while obj1 remains unchanged with "Microsoft"
+
+.................................................................................................................................................
+
+// Examples:
 const obj4 = {
     cat: "Abhi",
     age: "12",
